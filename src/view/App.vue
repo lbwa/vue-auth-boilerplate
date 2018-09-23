cd5e29d053c9179f09a2c6c5dcd877d21c0e09d5
Bowen <mr_lbw@outlook.com>
2018-09-23 08:28:35 +0800

    feat: add app-footer

--- src/view/App.vue
+++ src/view/App.vue
@@ -4,8 +4,9 @@
     <el-container>
       <app-header @toggleAside="toggleAside"></app-header>
       <router-view></router-view>
-      <el-footer>
-        footer
+      <el-footer class="footer__layout footer__font">
+        <svg height="24" class="octicon-mark" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+        Created By <a href="https://github.com/lbwa" class="author">Bowen</a>
       </el-footer>
     </el-container>
   </el-container>
@@ -37,4 +38,19 @@ export default {
 <style lang="sass" scoped>
 #root
   min-height: 100vh
+
+.footer
+  &__layout
+    display: flex
+    justify-content: center
+    align-items: center
+
+    .octicon-mark
+      margin-right: 10px
+      color: #c6cbd1 // #c6cbd1
+      height: 24px
+
+  &__font
+    color: rgba(0,0,0,.45)
+    font-size: 14px
 </style>
