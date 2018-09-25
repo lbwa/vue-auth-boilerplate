7058d347bf90291929f37ccb7bd4a17a7ddf4304
Bowen <mr_lbw@outlook.com>
2018-09-25 22:15:49 +0800

    fix: use hash mode rather than history mode

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,14 @@ import routes from './routes'
 
 Vue.use(VueRouter)
 
+/**
+ * 1. 在使用 history 模式的情况下，将导致嵌套路由出错，但 hash 模式下，正常；推测为后
+ * 端对应路由配置问题
+ * 2. 示例如：在 history 模式下先点击 dashboard 下条目再点击 form 下条目将报错，反
+ * 之，不成立
+ */
+// https://router.vuejs.org/zh/guide/essentials/history-mode.html
 export default new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   routes
 })
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,9 @@
 import components from './components'
 
-// ! FIXME: 先点击 dashboard 下条目再点击 form 下条目将报错，反之，不成立
 export default [
   {
     path: '/',
-    component: components.analysis
-    // redirect: '/form/basic-form'
+    redirect: '/dashboard/analysis'
   },
   {
     path: '/dashboard',
