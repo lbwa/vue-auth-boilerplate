439a7362f8fd9a3d9082c953a74cffc6f941ca92
Bowen <mr_lbw@outlook.com>
2018-10-02 20:22:06 +0800

    feat: bacis layout for workspace projects

--- src/view/Workspace.vue
+++ src/view/Workspace.vue
@@ -1,11 +1,22 @@
 <template>
   <el-main class="workspace">
     <workspace-header :current-user="currentUser"></workspace-header>
+    <el-row :gutter="24" class="workspace__projects__gutter">
+      <el-col
+        :xs="24" :sm="24" :md="24" :lg="24" :xl="12"
+      >
+        <workspace-projects></workspace-projects>
+      </el-col>
+      <el-col
+        :xs="24" :sm="24" :md="24" :lg="24" :xl="8"
+      ></el-col>
+    </el-row>
   </el-main>
 </template>
 
 <script>
-import WorkspaceHeader from 'COMPONENTS/WorkspaceHeader'
+import WorkspaceHeader from 'COMPONENTS/Workspace/Header'
+import WorkspaceProjects from 'COMPONENTS/Workspace/Projects'
 import { fetchUser } from 'SERVICES'
 import { mapState, mapMutations } from 'vuex'
 
@@ -27,11 +38,15 @@ export default {
   },
 
   components: {
-    WorkspaceHeader
+    WorkspaceHeader,
+    WorkspaceProjects
   }
 }
 </script>
 
 <style lang="sass" scoped>
-
+.workspace
+  &__projects
+    &__gutter
+      margin-top: 24px
 </style>
