1d5785721b221463655aba600631a4d0d0ee7583
Bowen <mr_lbw@outlook.com>
2018-10-05 23:50:00 +0800

    feat: add progress bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
 import routes from './routes'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   // mode: 'history',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  nprogress.start()
+  next()
+})
+
+router.afterEach((to, from) => {
+  nprogress.done()
+})
+
+export default router
