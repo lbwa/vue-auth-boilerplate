aaffe3874a227a5ef61a58aec6b3c38292937180
Bowen <mr_lbw@outlook.com>
2018-10-09 21:11:03 +0800

    chore: adjust Access/* tips

--- src/view/Access/Admin.vue
+++ src/view/Access/Admin.vue
@@ -1,15 +1,26 @@
 <template>
-  <el-main class="access__admin">
-    {{info}}
+  <el-main class="access__user">
+    <div class="access__description">
+      <h2 class="access__description__title">current role: {{role}}</h2>
+      <p class="access__description__main">{{info}}</p>
+    </div>
   </el-main>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
-      info: 'access__admin: This page only work with `admin` role.'
+      info: 'access__user: This private page only work with `admin` role.\nThe admin page will disappear when current role is not `admin`.'
     }
+  },
+
+  computed: {
+    ...mapState('login', [
+      'role'
+    ])
   }
 }
 </script>
--- src/view/Access/User.vue
+++ src/view/Access/User.vue
@@ -1,15 +1,26 @@
 <template>
   <el-main class="access__user">
-    {{info}}
+    <div class="access__description">
+      <h2 class="access__description__title">current role: {{role}}</h2>
+      <p class="access__description__main">{{info}}</p>
+    </div>
   </el-main>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data () {
     return {
-      info: 'access__user: This page work with role including `user`.'
+      info: 'access__user: This common page is from dynamic routes.'
     }
+  },
+
+  computed: {
+    ...mapState('login', [
+      'role'
+    ])
   }
 }
 </script>
