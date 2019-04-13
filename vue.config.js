864662ce40b428cbe68e9e3a5d369432b6714dd3
lbwa <mr_lbw@outlook.com>
2019-04-13 09:46:13 +0800

    build: optimize UI & css chunks

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const PATH = require('./config/path')
 const __DEV__ = process.env.NODE_ENV === 'development'
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/adminize/' : '/',
+  publicPath: !__DEV__ ? '/adminize/' : '/',
   productionSourceMap: false, // turn off source map
   configureWebpack(config) {
     if (!__DEV__) {
@@ -13,6 +13,45 @@ module.exports = {
   },
   chainWebpack(chainConfig) {
     aliasCreator(chainConfig)
+
+    chainConfig.when(!__DEV__, chainConfig => {
+      // Optimize chunks under production mode
+      chainConfig.optimization.splitChunks({
+        minSize: 30000, // 30kb
+        cacheGroups: {
+          // extract ui to create a chunk for long-term caching
+          ui: {
+            name: 'element-ui',
+            test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+            priority: -5, // more then `vendors` chunk priority
+            chunks: 'all' // to optimize sync and async chunks
+          },
+          vendors: {
+            name: 'chunk-vendors',
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            chunks: 'initial'
+          },
+          common: {
+            name: 'chunk-common',
+            minChunks: 2,
+            priority: -20,
+            chunks: 'initial',
+            reuseExistingChunk: true
+          },
+          // ref: https://github.com/webpack-contrib/mini-css-extract-plugin#extracting-css-based-on-entry
+          styles: {
+            name: 'chunk-styles',
+            test: (m, c, entry = 'app') =>
+              m.constructor.name === 'CssModule' &&
+              recursiveIssuer(m) === entry,
+            chunks: 'all', // to optimize sync and async chunks
+            enforce: true
+          }
+        }
+      })
+      // chainConfig.module.rule('js').use('babel-loader')
+    })
   }
 }
 
@@ -31,4 +70,14 @@ function aliasCreator(chainConfig) {
     .set('PERMISSION', path.resolve(PATH.SOURCE_PATH, './permission'))
     .set('ASSETS', path.resolve(PATH.SOURCE_PATH, './assets'))
     .set('LANG', path.resolve(PATH.SOURCE_PATH, './lang'))
+}
+
+function recursiveIssuer(m) {
+  if (m.issuer) {
+    return recursiveIssuer(m.issuer)
+  } else if (m.name) {
+    return m.name
+  } else {
+    return false
+  }
 }
