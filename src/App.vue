f698c0dadd053708221bb5f6551fc937925cb855
lbwa <mr_lbw@outlook.com>
2020-02-20 20:47:46 +0800

    feat: layout system

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <transition name="layout">
+      <component :is="layoutComponent">
+        <router-view />
+      </component>
+    </transition>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { DEFAULT_LAYOUT } from './constants'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'App',
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  computed: {
+    layoutComponent() {
+      // for trigger framework internal dep collection, make `layoutComponent`
+      // reactive
+      const layout = this.$route.meta.layout || DEFAULT_LAYOUT
+      // A component name should be embedded by template string directly.
+      // Otherwise, `Critical dependency: the request of a dependency is an
+      // expression` warning occurred.
+      return () =>
+        import(
+          /* webpackChunkName: 'layout-[request]' */ `./layouts/${layout}.vue`
+        )
     }
   }
 }
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
 </style>
---
+++ src/layouts/LMainWithFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="l-mwf">
+    <router-view />
+    <footer class="l-mwf__footer">
+      Copyright &copy; {{ new Date().getFullYear() }}
+      <a href="http://github.com/lbwa" target="_blank" rel="noopener noreferrer"
+        >Bowen</a
+      >
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LMainWithFooter'
+}
+</script>
+
+<style lang="sass" scoped>
+.l-mwf
+  &__footer
+    padding: 20px 0
+    font-size: 12px
+    text-align: center
+</style>
