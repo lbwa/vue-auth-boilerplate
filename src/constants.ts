499450e8ac67db31142bb9f32617561e1693b910
lbwa <mr_lbw@outlook.com>
2020-02-21 11:05:12 +0800

    feat(history): impl history linked list

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,12 @@
+/**
+ * This file is used to record all **symbolic constants**.
+ */
+
+/**
+ * the root router-view layout
+ */
 export const DEFAULT_LAYOUT = 'LMainWithFooter'
+/**
+ * the max value of history records and should be greater than 1
+ */
+export const RECORD_MAX_VAL = 20
---
+++ src/store/modules/history.ts
@@ -0,0 +1,84 @@
+import { Module } from 'vuex'
+import { Route } from 'vue-router'
+import invariant from 'tiny-invariant'
+import { RootState } from '../index'
+import { RECORD_MAX_VAL } from '../../constants'
+
+export type RecordItem = {
+  name?: string
+  fullPath: string
+  next: RecordItem | null
+}
+
+interface RecordItemConstructor {
+  (this: RecordItem, route: Route, next?: RecordItem): void
+  new (route: Route, next?: RecordItem): RecordItem
+}
+
+const RecordItem = function(this: RecordItem, route: Route, next?: RecordItem) {
+  if (route.name) this.name = route.name
+  this.fullPath = route.fullPath
+  this.next = next || null
+} as RecordItemConstructor
+
+const history: Module<Record<'recordHead', RecordItem | null>, RootState> = {
+  namespaced: true,
+
+  state: {
+    recordHead: null
+  },
+
+  mutations: {
+    append(state, route: Route) {
+      if (!state.recordHead) {
+        state.recordHead = new RecordItem(route)
+        return
+      }
+
+      let current: RecordItem | null = state.recordHead
+      let size = 1
+      invariant(
+        RECORD_MAX_VAL > 1,
+        '[module/history]: RECORD_MAX_VAL should be greater then 1'
+      )
+      while (current.next) {
+        ++size
+        current = current.next
+      }
+      // drop the head when exceed
+      if (size === RECORD_MAX_VAL) {
+        state.recordHead = state.recordHead.next
+      }
+      // a new tail element of linked list
+      current.next = new RecordItem(route)
+    },
+
+    prepend(state, route: Route) {
+      if (!state.recordHead) {
+        state.recordHead = new RecordItem(route)
+        return
+      }
+
+      let current: RecordItem | null = state.recordHead
+      let size = 1
+      invariant(
+        RECORD_MAX_VAL > 1,
+        '[module/history]: RECORD_MAX_VAL should be greater then 1'
+      )
+
+      state.recordHead = new RecordItem(route, state.recordHead)
+
+      while (current.next) {
+        ++size
+        if (size === RECORD_MAX_VAL) {
+          // drop the tail element
+          current.next = null
+          break
+        }
+        current = current.next
+      }
+    }
+  }
+}
+
+export default history
