c25f3f9e4d4692aa01fb7a20ee9985fc36e3b2ad
lbwa <mr_lbw@outlook.com>
2020-02-23 15:58:35 +0800

    feat: add User Error Login views

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,43 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue')
+    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    meta: {}
+  },
+  {
+    path: '/login',
+    name: '/login',
+    component: () =>
+      import(/* webpackChunkName: 'login' */ '../views/Login/index.vue')
+  },
+  {
+    path: '/forbidden',
+    name: 'ErrorForbidden',
+    component: () =>
+      import(
+        /* webpackChunkName: 'error-forbidden' */ '../views/Error/index.vue'
+      ),
+    props: {
+      code: 403,
+      message: 'Forbidden access'
+    }
+  },
+  {
+    path: '*',
+    name: 'ErrorNotFound',
+    component: () =>
+      import(
+        /* webpackChunkName: 'error-not-found' */ '../views/Error/index.vue'
+      ),
+
+    props: {
+      code: 404,
+      message: 'Seems nothing could be found.'
+    }
   }
 ]
 
