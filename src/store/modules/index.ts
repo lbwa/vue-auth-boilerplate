b1a38bce3caaaed7eae9861e53f4c645ba631855
lbwa <mr_lbw@outlook.com>
2020-02-29 19:58:29 +0800

    feat(store): import store module automatically

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,5 +1,16 @@
-import history from './history'
+import { Module } from 'vuex'
+import { RootState } from '..'
 
-export default {
-  history
-}
+const requireStoreModule = require.context(
+  '.',
+  false,
+  /^\.\/(?!index)\w+\.(t|j)s/ // match all ts file without index.ts
+)
+
+export default requireStoreModule.keys().reduce((map, current) => {
+  const [, moduleName] = /\.\/([\w-]+)\.ts/.exec(current) || []
+  if (!map[moduleName]) {
+    map[moduleName] = requireStoreModule(current).default
+  }
+  return map
+}, {} as Record<string, Module<object, RootState>>)
