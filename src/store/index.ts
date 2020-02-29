6c1a96798eb09328e83a3f7ed79e972281e1d179
lbwa <mr_lbw@outlook.com>
2020-02-29 16:42:32 +0800

    feat(store): register store module automatically

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createLogger from 'vuex/dist/logger'
 import modules from './modules'
+import { createAbilityPlugin } from '../plugins/ability'
+import router from '@/router'
 
 Vue.use(Vuex)
 
@@ -9,11 +11,27 @@ const __DEV__ = process.env.NODE_ENV === 'development'
 
 export type RootState = {}
 
+const abilityPlugin = createAbilityPlugin(
+  'user/setUserAbilities',
+  router,
+  '/forbidden',
+  [
+    {
+      path: '/user',
+      component: () =>
+        import(/* webpackChunkName: 'private' */ '../views/User/index.vue'),
+      meta: {
+        ability: 'github.repo.read'
+      }
+    }
+  ]
+)
+
 export default new Vuex.Store<RootState>({
   state: {},
   mutations: {},
   actions: {},
   modules,
   strict: __DEV__,
-  plugins: __DEV__ ? [createLogger()] : []
+  plugins: __DEV__ ? [createLogger(), abilityPlugin] : [abilityPlugin]
 })
