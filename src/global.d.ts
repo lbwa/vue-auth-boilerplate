2bc132a22459f11ed740f59ea9631ec52fea888c
lbwa <mr_lbw@outlook.com>
2020-02-29 17:36:56 +0800

    feat(flags): define feature flags

---
+++ src/global.d.ts
@@ -0,0 +1,14 @@
+/**
+ * DO NOT import/export any variables from this file for keeping variables globally
+ *
+ * In TypeScript,  any file containing a top-level import or export is
+ * considered a module. Conversely, a file without any top-level import or
+ * export declarations is treated as a script whose contents are available in
+ * the global scope (and therefore to modules as well).
+ * @doc https://www.typescriptlang.org/docs/handbook/modules.html
+ */
+
+/**
+ * Whether we are in development environment
+ */
+declare const __DEV__: boolean
