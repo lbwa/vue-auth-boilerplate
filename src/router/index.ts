9670c77f75d354c7732f286e2ddc9433162e8ef1
lbwa <mr_lbw@outlook.com>
2020-02-29 20:20:23 +0800

    feat(router): guard for detecting login state

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import { onLogin } from './guards'
 
 Vue.use(VueRouter)
 
@@ -12,13 +13,13 @@ export const routes = [
   },
   {
     path: '/login',
-    name: '/login',
+    name: 'Login',
     component: () =>
       import(/* webpackChunkName: 'login' */ '../views/Login/index.vue')
   },
   {
     path: '/forbidden',
-    name: 'ErrorForbidden',
+    name: 'ForbiddenError',
     component: () =>
       import(
         /* webpackChunkName: 'error-forbidden' */ '../views/Error/index.vue'
@@ -30,7 +31,7 @@ export const routes = [
   },
   {
     path: '*',
-    name: 'ErrorNotFound',
+    name: 'NotFoundError',
     component: () =>
       import(
         /* webpackChunkName: 'error-not-found' */ '../views/Error/index.vue'
@@ -46,5 +47,7 @@ export const routes = [
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach(onLogin)
 
 export default router
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,5 @@
+export const errorLog = console.error
+
 export function isDef<T>(val: T): val is NonNullable<T> {
   return val !== null && val !== undefined
 }
