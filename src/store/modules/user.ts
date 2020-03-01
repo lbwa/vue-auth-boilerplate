521a0003326a344cafa0417c8dc6352ed1cc3a77
lbwa <mr_lbw@outlook.com>
2020-03-01 20:02:38 +0800

    feat(login): impl how to fetch user abilities

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,6 +1,7 @@
 import { Module } from 'vuex'
 import { RootState } from '..'
 import { Ability } from 'v-access'
+import { userLogin, fetchUserAbilities } from '@/effects'
 
 interface UserState {
   token: string
@@ -37,47 +38,17 @@ const user: Module<UserState, RootState> = {
 
   actions: {
     async login({ commit }, { username, password }: Record<string, string>) {
-      const { token } = await new Promise<{
-        code: number
-        [key: string]: unknown
-      }>((resolve, reject) => {
-        setTimeout(() => {
-          if (
-            (username === 'admin' && password === 'admin') ||
-            (username === 'user' && password === 'user')
-          ) {
-            return resolve({
-              code: 200,
-              token:
-                Math.random()
-                  .toString(16)
-                  .slice(2) +
-                username +
-                password
-            })
-          }
-          reject({
-            code: 401,
-            msg: 'account error'
-          })
-        }, 2 * 1000)
-      })
-      commit('setToken', token)
+      const { token } = await userLogin(username, password)
+      token && commit('setToken', token)
     },
     async fetchUserAbilities({ commit }) {
-      const { abilities } = await Promise.resolve({
-        abilities: Array(20)
-          .fill(null)
-          .map((_, index) => ({
-            id: `ability.simulator.${index}`,
-            name: `ability.simulator.${index}`,
-            createAt: Date.now() + index
-          }))
-      })
-      commit(
-        'setUserAbilities',
-        abilities.map(ability => ability.id)
-      )
+      const { abilities } = await fetchUserAbilities()
+      abilities &&
+        abilities.length &&
+        commit(
+          'setUserAbilities',
+          abilities.map((ability: Record<'id', string>) => ability.id)
+        )
     }
   }
 }
