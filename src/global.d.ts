e0ad8770226545b6119a6eade1eb5f99b238cb2e
lbwa <mr_lbw@outlook.com>
2020-03-01 16:59:04 +0800

    feat(sass): support css module with TS render func

--- src/global.d.ts
+++ src/global.d.ts
@@ -12,3 +12,23 @@
  * Whether we are in development environment
  */
 declare const __DEV__: boolean
+
+/**
+ *
+ * https://github.com/facebook/create-react-app/blob/v3.4.0/packages/react-scripts/lib/react-app.d.ts#L53-L66
+ */
+
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
