f24322467859d0cb23e7a72737251aa946357e5b
lbwa <mr_lbw@outlook.com>
2020-03-01 17:02:48 +0800

    feat(constants): combine all symbolic constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,13 @@
  * the root router-view layout
  */
 export const DEFAULT_LAYOUT = 'LMainFooter'
+
 /**
  * the max value of history records and should be greater than 1
  */
 export const RECORD_MAX_VAL = 20
+
+/**
+ * Used to forbid unauthorized access
+ */
+export const FORBIDDEN_ROUTE = '/forbidden'
