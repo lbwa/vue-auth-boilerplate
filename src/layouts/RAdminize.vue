57f1d59266df785ea3c4f074dc64b7a897e14cf7
lbwa <mr_lbw@outlook.com>
2020-03-01 23:45:42 +0800

    feat(RAdminize): impl RAdminize layout component

---
+++ src/layouts/RAdminize.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="isOpenDrawer" app clipped>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="isOpenDrawer = !isOpenDrawer" />
+      <v-toolbar-title>Adminize</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <!-- router view export -->
+        <router-view />
+      </v-container>
+    </v-content>
+
+    <base-footer app />
+  </v-app>
+</template>
+
+<script>
+import BaseFooter from '../components/BaseFooter'
+
+export default {
+  name: 'RAdminize',
+
+  data() {
+    return {
+      isOpenDrawer: true
+    }
+  },
+
+  components: {
+    BaseFooter
+  },
+
+  created() {
+    this.$vuetify.theme.dark = true
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
