9a1b1a48ecb587b394180dfa93395b62e7de103d
lbwa <mr_lbw@outlook.com>
2020-03-04 21:19:59 +0800

    fix(app): avoid redundant re-rendering with entire app
    Never use this.$route.meta.layout directly
    
    Because layoutComponent's underlying calculation depends on this.$route,
    and this.$route changes will occure all subscriber though
    this.$route.meta.layout never changes.
    
    * register period
    https://github.com/vuejs/vue-router/blob/v3.1.6/src/install.js#L27
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/global-api/index.js#L41
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/util/index.js#L11
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/index.js#L135
    
    * subscribe period
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/instance/state.js#L187
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/instance/state.js#L249
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/watcher.js#L95
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/watcher.js#L102
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/dep.js#L58-L61
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/watcher.js#L221
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/dep.js#L31-L35
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/watcher.js#L128-L137
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/dep.js#L24
    
    * publish period
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/index.js#L191
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/dep.js#L37-L49
    https://github.com/vuejs/vue/blob/v2.6.11/src/core/observer/watcher.js#L164-L173

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,21 @@ import { DEFAULT_LAYOUT } from './constants'
 export default Vue.extend({
   name: 'App',
 
+  data() {
+    return {
+      layoutType: DEFAULT_LAYOUT
+    }
+  },
+
   computed: {
     layoutComponent() {
-      // for trigger framework's internal dep collection, make `layoutComponent`
-      // reactive
-      const layout = this.$route.meta.layout || DEFAULT_LAYOUT
+      // 1. For trigger framework's internal dep collection, make
+      // `layoutComponent` reactive
+      // 2. DON'T use `this.$route.meta.layout` directly.
+      // Because layoutComponent's underlying calculation depends on this.$route
+      // this.$route changes will occur property re-computed
+      // though this.$route.meta.layout never changes.
+      const layout = this.layoutType
       // A component name should be embedded by template string directly.
       // Otherwise, `Critical dependency: the request of a dependency is an
       // expression` warning occurred.
@@ -26,6 +36,7 @@ export default Vue.extend({
 
   watch: {
     $route: function(to) {
+      this.layoutType = to.meta.layout || DEFAULT_LAYOUT
       this.$store.commit('history/append', to)
     }
   }
