2b5c7d4a9c029563cc789318b0b199eda6822b57
lbwa <mr_lbw@outlook.com>
2020-03-07 17:59:11 +0800

    feat(store): don't use replaceState anymore
    replaceState API wouldn't trigger vue-devtool

--- src/plugins/store.ts
+++ src/plugins/store.ts
@@ -2,13 +2,13 @@ import { Plugin } from 'vuex'
 import { RootState } from '@/store/global'
 import cloneDeep from 'lodash.clonedeep'
 
-export const createResetPlugin: (
-  listenType: string
-) => Plugin<RootState> = listenType => store => {
-  const stateClone = cloneDeep(store.state)
-
-  store.subscribeAction(action => {
-    if (action.type !== listenType) return
-    store.replaceState(stateClone)
-  })
+/**
+ * For storing the whole initial `store.state` including all modules states
+ * @when This plugin will be called only once in the whole Store instance lifetime
+ * @see https://github.com/vuejs/vuex/blob/v3.1.2/src/store.js#L64-L65
+ */
+export const createResetPlugin: () => Plugin<RootState> = () => store => {
+  const clone = cloneDeep(store.state)
+  delete clone.DO_NOT_MUTATE
+  store.commit('setSnapshot', clone)
 }
