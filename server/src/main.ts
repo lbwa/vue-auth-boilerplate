c6c36a2796dd020f42c8b4be2ee38fffaba5a5a5
lbwa <mr_lbw@outlook.com>
2020-03-08 15:32:28 +0800

    mock(server): add response interceptor

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ResponseInterceptor } from './response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.useGlobalInterceptors(new ResponseInterceptor())
   await app.listen(3000)
 }
 bootstrap()
