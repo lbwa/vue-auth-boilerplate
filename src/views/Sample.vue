e0d7bf3eb72fdbcc89d1aecb1327aa6b3bf7006f
lbwa <mr_lbw@outlook.com>
2020-03-28 23:49:06 +0800

    feat: add Sample page

---
+++ src/views/Sample.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sample"><h2>Sample</h2></div>
+</template>
+
+<script>
+export default {
+  name: 'Sample'
+}
+</script>
+
+<style lang="sass" scoped>
+.sample
+  width: 100%
+  height: 100%
+</style>
