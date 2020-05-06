fa2484f8d127c1a15634bb18eec8b55ea52cb7f2
Bowen Liu <mr_lbw@outlook.com>
2020-05-06 23:02:43 +0800

    feat(html): inject build time and commit hash

--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,16 @@ const appVersion = require('./package.json').version
 
 const __DEV__ = process.env.NODE_ENV === 'development'
 const __PROD__ = process.env.NODE_ENV === 'production'
+const commitHash = { value: '' }
+
+try {
+  commitHash.value = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString()
+} catch (err) {
+  // current workspace isn't git repository
+  commitHash.value = ''
+}
 
 // should we enable preload feature for CDN files
 const shouldPreloadCDNFiles = true
@@ -26,16 +36,7 @@ module.exports = {
       args.__DEV__ = JSON.stringify(__DEV__)
       args.__BUILD_TIME__ = JSON.stringify(new Date().toString())
       args.__VERSION__ = JSON.stringify(appVersion)
-      try {
-        args.__COMMIT_HASH__ = JSON.stringify(
-          require('child_process')
-            .execSync('git rev-parse HEAD')
-            .toString()
-        )
-      } catch (err) {
-        // works without git
-        args.__COMMIT_HASH__ = JSON.stringify('')
-      }
+      args.__COMMIT_HASH__ = JSON.stringify(commitHash.value)
       return [args]
     })
 
@@ -54,6 +55,8 @@ module.exports = {
         args.cdnJsUrls = thirdPartiesUrls.js || []
         args.shouldPreloadCDNFiles = shouldPreloadCDNFiles
         args.appVersion = appVersion
+        args.buildTime = new Date().toLocaleString()
+        args.commitHash = commitHash.value
         return [args]
       })
 
